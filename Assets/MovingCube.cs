85494aaaf2b57e11e3e75b4a8e186bf7e7cfa7c6
hktrollgames <hktrollgames@gmail.com>
2022-08-14 15:31:49 +0900

    그라디언트 쉐이더, 움직이는 큐브

--- Assets/MovingCube.cs
+++ Assets/MovingCube.cs
@@ -1,18 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 pivot;
+    [SerializeField] Vector3 desPoint;    // 목표지점.
+    [SerializeField] Vector3 startPoint;  // 시작 지점.
+    float startTime;
     void Start()
     {
-        
+        startPoint = new Vector3(transform.localPosition.x, transform.position.y, transform.localPosition.z);
+        desPoint = new Vector3(-startPoint.x, startPoint.y, -startPoint.z);
+        pivot.y = 0;
+        startPoint += pivot;
+        desPoint += pivot;
+        startTime = Time.time;
     }
 
-    // Update is called once per frame
+    public float elapsTime;
+    public float 나머지빼기1;
+    public float time;
     void Update()
     {
-        
+        elapsTime = Time.time - startTime;
+        나머지빼기1 = elapsTime % 2 - 1f;
+        time = Mathf.Abs(나머지빼기1); // 1 ~ 0 ~ 1
+        //a + (b - a) * t.
+        Vector3 pos = Vector3.Lerp(desPoint, startPoint, time);
+        transform.localPosition = pos;
     }
 }
